1f92d7c0906a258a4c29817b99deb211c1612d2d
c07d-736734 <coldshots143@gmail.com>
2017-11-08 22:21:07 +0800

    11-8-2017 10:21pm

--- models/models.js
+++ models/models.js
@@ -8,25 +8,25 @@ mongoose.Promise = global.Promise;
 */
 mongoose.model("coins_users",
                 mongoose.Schema({
-                            _username:{type:String,required:true,unique:true,dropDups:true},
-                            _password:{type:String,required:true},
-                            _fname:{type:String,required:true},
-                            _lname:{type:String,required:true},
-                            _email:{type:String,required:true},
-                            _btc_add:{type:String,required:true},
-                            _status:{type:String,required:true},
-                            _type:{type:String,required:true},
-                            _upline:{type:mongoose.Schema.Types.ObjectId,ref:'coins_users',required:false},
-                            _created:{type:Date,default:Date.now}
+                        _username:{type:String,required:true,unique:true,dropDups:true},
+                        _password:{type:String,required:true},
+                        _fname:{type:String,required:true},
+                        _lname:{type:String,required:true},
+                        _email:{type:String,required:true},
+                        _btc_add:{type:String,required:true},
+                        _status:{type:String,required:true},
+                        _type:{type:String,required:true},
+                        _upline:{type:mongoose.Schema.Types.ObjectId,ref:'coins_users',required:false,default:null},
+                        _created:{type:Date,default:Date.now}
                         })
                 );
                 
 
 mongoose.model("coins_dummy_btc",
                 mongoose.Schema({
-                                _btc_add:{type:String,required:true},
-                                _status:{type:String,required:true}
-                })
+                        _btc_add:{type:String,required:true},
+                        _status:{type:String,required:true}
+                        })
             );
 
 
@@ -37,7 +37,7 @@ mongoose.model("coins_released",
                         _date_release:{type:Date,default:Date.now},
                         _maximum_coins:{type:Number,required:true},//coins per user to sold
                         _minimum_coins:{type:Number,required:true}//minimum coins to be sold
-               })
+                    })
             );
 
 
@@ -54,12 +54,12 @@ mongoose.model("coins_reserved",
 
 
 mongoose.model("coins_wallet",
-              mongoose.Schema({
-                  _owner:{type:mongoose.Schema.Types.ObjectId,ref:'coins_users',required:false},
-                  _balance:{type:Number,default:0},
-                  _tx_amount:{type:Number,default:0},
-                  _type:{type:String,requried:true},
-                  _created:{type:Date,default:Date.now}
+                mongoose.Schema({
+                        _owner:{type:mongoose.Schema.Types.ObjectId,ref:'coins_users',required:false},
+                        _balance:{type:Number,default:0},
+                        _tx_amount:{type:Number,default:0},
+                        _type:{type:String,requried:true},
+                        _created:{type:Date,default:Date.now}
               })
             );
 
